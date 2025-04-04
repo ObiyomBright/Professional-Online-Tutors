3ee5d59192c23cf6502be89bbc9731002b67b731
Obiyom Bright <brightobiyom2@gmail.com>
2025-04-04 06:09:14 +0100

    ...

--- index.js
+++ index.js
@@ -12,3 +12,43 @@ document.querySelectorAll('.nav-links a').forEach(anchor => {
         document.querySelector('.nav-links').classList.remove('active'); // Close menu on click
     });
 });
+
+document.addEventListener('DOMContentLoaded', ()=> {
+
+    const bookSessionBtn = document.getElementById('bookSessionBtn');
+const bookSessionContainer = document.querySelector('.book-session-container');
+
+bookSessionBtn.addEventListener('click', e => {
+    bookSessionContainer.style.display = 'block';
+})
+
+    document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const form= e.target;
+    const formData = new FormData(form);
+
+    //Bundle form data
+    const  gradeLevel = formData.get('grade_level');
+    const subjects = formData.get('subjects[]').join(', ') || 'None';
+    const classDuration = formData.get('class_duration');
+    const numSections = formData.get('num_sections');
+    const studentName = formData.get('student_name');
+    const email= formData.get('email') || 'None';
+    const phone = formData.get('phone');
+
+    const bundledText = `
+        New Online Class Request 
+        Student name: ${studentName}
+        Email: ${email}
+        Phone: ${phone}
+
+        Grade level: ${gradeLevel}
+        Subjects of interest: ${subjects}
+        Class Duration: ${classDuration}
+        Number of Section: ${numSections}   
+    `.trim();
+
+    console.log(bundledText);
+})
+})
